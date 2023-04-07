687f2bc9a352fd572501b91d5b2f02a286dcdb04
FujihiraKazuaki <117697346+FujihiraKazuaki@users.noreply.github.com>
2023-04-07 23:38:32 +0900

    Update kintai_change_finish_remote.py

--- scripts/kintai_change_finish_remote.py
+++ scripts/kintai_change_finish_remote.py
@@ -49,76 +49,76 @@ import user_info
 # #ウインドウサイズを変更
 # driver.set_window_size(1920,1080)
 
-# Firefox
-options = Options()
-firefox_profile = "./zcy7yqlt.default-release-1679651078178" #Firefoxのprofileのパスを指定
-fp = webdriver.FirefoxProfile(firefox_profile)
-options.headless = True
-firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
-driver = webdriver.Firefox(options=options,firefox_profile=fp,capabilities=firefox_capabilities)
-driver.set_window_size(1920, 1080)
+# # Firefox
+# options = Options()
+# firefox_profile = "./zcy7yqlt.default-release-1679651078178" #Firefoxのprofileのパスを指定
+# fp = webdriver.FirefoxProfile(firefox_profile)
+# options.headless = True
+# firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
+# driver = webdriver.Firefox(options=options,firefox_profile=fp,capabilities=firefox_capabilities)
+# driver.set_window_size(1920, 1080)
 
-# Googleアクセス
-driver.get('https://www.google.com/?hl=ja')
+# # Googleアクセス
+# driver.get('https://www.google.com/?hl=ja')
 
-#ログイン開始
-try:
-  #ここからSSO処理
-  time.sleep(5)
-  elm = driver.find_element_by_xpath("//*[@class='gb_e']")
-  actions = ActionChains(driver)
-  actions.move_to_element(elm)
-  actions.perform()
-  actions.reset_actions()
-  driver.find_element_by_xpath("//*[@aria-label='Google アプリ']").click()
+# #ログイン開始
+# try:
+#   #ここからSSO処理
+#   time.sleep(5)
+#   elm = driver.find_element_by_xpath("//*[@class='gb_e']")
+#   actions = ActionChains(driver)
+#   actions.move_to_element(elm)
+#   actions.perform()
+#   actions.reset_actions()
+#   driver.find_element_by_xpath("//*[@aria-label='Google アプリ']").click()
 
-  time.sleep(5)
-  iframe = driver.find_element_by_xpath("//iframe[@role='presentation']")
-  driver.switch_to.frame(iframe)
-  time.sleep(5)
-  elm = driver.find_element_by_xpath("//*[contains(text(), 'TeamSpirit')]")
-  driver.execute_script("window.scrollTo(0, " + str(elm.location['y']) + ");")
-  driver.find_element_by_xpath("//*[contains(text(), 'TeamSpirit')]").click()
-  time.sleep(5)
-  handle_array = driver.window_handles
-  driver.switch_to.window(handle_array[1])
+#   time.sleep(5)
+#   iframe = driver.find_element_by_xpath("//iframe[@role='presentation']")
+#   driver.switch_to.frame(iframe)
+#   time.sleep(5)
+#   elm = driver.find_element_by_xpath("//*[contains(text(), 'TeamSpirit')]")
+#   driver.execute_script("window.scrollTo(0, " + str(elm.location['y']) + ");")
+#   driver.find_element_by_xpath("//*[contains(text(), 'TeamSpirit')]").click()
+#   time.sleep(5)
+#   handle_array = driver.window_handles
+#   driver.switch_to.window(handle_array[1])
   
-  #お知らせウィンドウが開いていた場合は閉じる
-  notification_window = driver.find_elements_by_xpath("//div[@data-dojo-attach-point='titleBar']/*[contains(text(), 'お知らせ')]")
+#   #お知らせウィンドウが開いていた場合は閉じる
+#   notification_window = driver.find_elements_by_xpath("//div[@data-dojo-attach-point='titleBar']/*[contains(text(), 'お知らせ')]")
   
-  if notification_window:
-    y_loca = driver.find_element_by_xpath("//tr[@id='dialogInfoBottom']//button[@class='std-button2 close_button']")
-    driver.execute_script("window.scrollTo(0, " + str(y_loca.location['y']) + ");")
-    y_loca.click()
-  else:
-    pass
+#   if notification_window:
+#     y_loca = driver.find_element_by_xpath("//tr[@id='dialogInfoBottom']//button[@class='std-button2 close_button']")
+#     driver.execute_script("window.scrollTo(0, " + str(y_loca.location['y']) + ");")
+#     y_loca.click()
+#   else:
+#     pass
     
-  time.sleep(5)
-  elm = driver.find_elements_by_xpath("//a[@title='勤務表タブ']")
-  if elm :
-    pass 
-  else :
-    raise ValueError("ログインに失敗しました")
+#   time.sleep(5)
+#   elm = driver.find_elements_by_xpath("//a[@title='勤務表タブ']")
+#   if elm :
+#     pass 
+#   else :
+#     raise ValueError("ログインに失敗しました")
 
-except NoSuchElementException as e:
-  print(e)
+# except NoSuchElementException as e:
+#   print(e)
 
-print("ログイン完了しました")
-time.sleep(7)
+# print("ログイン完了しました")
+# time.sleep(7)
 
-print("処理開始します。")
-#iframeを切り替える
-iframe=driver.find_element_by_xpath("//*[@id='0665F00000117vk']")
-driver.switch_to.frame(iframe)
-driver.implicitly_wait(15)
+# print("処理開始します。")
+# #iframeを切り替える
+# iframe=driver.find_element_by_xpath("//*[@id='0665F00000117vk']")
+# driver.switch_to.frame(iframe)
+# driver.implicitly_wait(15)
 
-#退勤ボタンをクリック
-y_loca = driver.find_element_by_xpath("//*[@id='btnEtInput']")
-driver.execute_script("window.scrollTo(0, " + str(y_loca.location['y']) + ");")
-y_loca.click()
-time.sleep(2)
+# #退勤ボタンをクリック
+# y_loca = driver.find_element_by_xpath("//*[@id='btnEtInput']")
+# driver.execute_script("window.scrollTo(0, " + str(y_loca.location['y']) + ");")
+# y_loca.click()
+# time.sleep(2)
 
-driver.switch_to.default_content()
+# driver.switch_to.default_content()
 
 #完了処理
 print("処理が正常に完了しました。")
